34c71c609489bf0d701bc38ea567a09e7bf35637
LaylaGloom <ruthnhm.30@gmail.com>
2018-03-23 00:18:56 -0600

    Cambio de estilo a logi, y cambios en hojas de estilos dino.css y style.css

--- application/views/login.php
+++ application/views/login.php
@@ -1,10 +1,16 @@
-    <div id="form">
+    <center>
+      <div class="titulo container-fluid">
+        <h3>Iniciar sesión</h3>
+      </div>
+    <div class="login" id="form">
       <form id="login" action="<?= base_url('Home/Log') ?>">
-        <input type="text" id="nick" class="form-control" placeholder="Usuario">
+        <input type="text" id="nick" class="form-control inputs" placeholder="Usuario">
         <br>
-        <input type="password" id="pass" class="form-control" placeholder="Contraseña">
+        <input type="password" id="pass" class="form-control inputs" placeholder="Contraseña">
         <br>
         <br>
-        <input type="submit" class="form-control btn btn-success">
+        <input type="submit" class="form-control btn btn-success btn-base">
+
       </form>
     </div>
+      </center>
